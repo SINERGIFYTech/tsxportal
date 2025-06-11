47f98b193853634912fdca713bddbdbb0b774f7a
Carlos Perez <carlosaperez1997@gmail.com>
2025-06-11 16:47:26 -0500

    ajuste y filtro de transacciones y de ingresos con tether

--- server/utils/deposit.js
+++ server/utils/deposit.js
@@ -1,41 +1,33 @@
 const db = require("../db");
-const { encryptPrivateKey, decryptPrivateKey } = require("./encrypt");
 const TRC20Utils = require("./trc20");
 
 const depositUtils = {
   create: async ({ amount, clienteId }) => {
-    const walletData = await TRC20Utils.create();
+    const walletData = TRC20Utils.create();
     const depositBody = {
-      amount_expected: amount,
+      amount: amount,
       clienteId,
-      wallet_address: walletData.address.base58,
-      wallet_private_encrypted: encryptPrivateKey(walletData.privateKey),
-      wallet_hex_address: walletData.address.hex,
+      wallet_address: walletData,
     };
 
     await db.query(
       `
     INSERT INTO depositos 
-    (amount_expected, clienteId, wallet_address, wallet_private_encrypted, wallet_hex_address)
-    VALUES (?, ?, ?, ?, ?)
+    (amount, clienteId, wallet_address)
+    VALUES (?, ?, ?)
   `,
-      [
-        depositBody.amount_expected,
-        depositBody.clienteId,
-        depositBody.wallet_address,
-        depositBody.wallet_private_encrypted,
-        depositBody.wallet_hex_address,
-      ]
+      [depositBody.amount, depositBody.clienteId, depositBody.wallet_address]
     );
     return {
-        wallet_address: depositBody.wallet_address,
-        amount_expected: depositBody.amount_expected
+      wallet_address: depositBody.wallet_address,
+      amount_expected: depositBody.amount,
     };
   },
   checkAddress: async (address) => {
-    const dataAddress = await TRC20Utils.getBalance(address);
+    const dataAddress = await TRC20Utils.getBalance();
+    //
     return dataAddress;
-  }
+  },
 };
 
 module.exports = depositUtils;
--- server/utils/trc20.js
+++ server/utils/trc20.js
@@ -1,18 +1,48 @@
-const { TronWeb } = require("tronweb");
-
-const tronWeb = new TronWeb({
-  fullHost: "https://api.trongrid.io",
-});
-
+const fetch = require("node-fetch");
+const URL_BASE = `https://api.trongrid.io/v1/accounts/`;
+const LIMIT_TRANSACTION = 10;
+const WALLET_TRC20 = process.env.WALLET_TRC20;
 const TRC20Utils = {
-  create: async () => {
-    const account = await tronWeb.createAccount();
+  create: () => {
+    const account = WALLET_TRC20;
     return account;
   },
-  getBalance: async (address) => {
-    const balanceTRX = await tronWeb.trx.getBalance(address); // Devuelve en SUN
-    console.log("Balance TRX:", balanceTRX / 1_000_000);
-    return { balance: balanceTRX / 1_000_000 }
+  getBalance: async (address = WALLET_TRC20) => {
+    try {
+      const url = URL_BASE + address;
+      const res = await fetch(url);
+      const response = await res.json();
+      const cuenta = response.data?.[0];
+      if (!cuenta) return { trx: 0, usdt: 0 };
+      const trx = (cuenta.balance || 0) / 1_000_000;
+      // Buscar el balance de USDT en el array trc20
+      let usdt = 0;
+      const trc20List = cuenta.trc20 || [];
+      for (const token of trc20List) {
+        if (token["TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"]) {
+          usdt =
+            parseFloat(token["TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"]) / 1_000_000;
+          break;
+        }
+      }
+      const url_transactions = `${URL_BASE}${address}/transactions/trc20?limit=${LIMIT_TRANSACTION}`;
+      const resTransactions = await fetch(url_transactions);
+      const jsonTransactions = await resTransactions.json();
+      if (!jsonTransactions.data) return [];
+
+      // fiLtrar de las transacciones que sean recibidas, y mas recientes
+      const transactionsReceived = jsonTransactions.data
+        .filter((transaction) => transaction.to === WALLET_TRC20)
+        .map((transaction) => ({
+          from: transaction.from,
+          value: transaction.value / 1000000,
+          date: new Date(transaction.block_timestamp),
+        }));
+      return { trx, usdt, transactions: transactionsReceived };
+    } catch (error) {
+      console.error("Error al consultar saldo:", error);
+      return { trx: 0, usdt: 0, transactions: [] };
+    }
   },
 };
 
