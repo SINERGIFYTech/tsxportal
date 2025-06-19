41a461aa1d0e1a18fd114b05362bb72f70912b99
Carlos Andres Perez Combariza <carlosaperez1997@gmail.com>
2025-06-19 10:39:53 -0500

    ajuste de envs

--- server/db.js
+++ server/db.js
@@ -2,12 +2,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    // user: 'investor',
-    // password: 's3@4bGe9%MRNPap5iXQ#9F',
-    password: '',
-    database: 'sinergify_investments'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
 
